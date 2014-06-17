4b0f50a5583f1b64c887ce7031f26d318f7bdf3e
Jeffrey Dill <jeffdill2@gmail.com>
2014-06-17 08:21:45 -0400

    A fallback commit before I blew everything away and started over

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////// SETTING UP UI
+////////////////////////////////////////////////////////////
+$(window).resize(function() {
+	$('.header').css('border-left-width', $(window).width());
+	$('.header').css('border-top-width', $(window).width() * 0.156666);
+});
+
+Parse.initialize("TJwmttIazJ3YEVm0bEW5KdReglYz0ViHpygK8y40", "9rTc1AIV0UHOIbaM8TMZBngR0H8ZzI6DOOzTXgwo");
+
+var Animal = Parse.Object.extend({
+	className: "Animal",
+});
+
+var doggy = new Animal();
+
+// doggy.save({
+// 	name: "Bones",
+// 	test: {array: [1,2,3]}
+// }, {
+// 	success: function() {
+// 		console.log('HUZZAH!');
+// 	},
+// 	error: function() {
+// 		console.log('OOPS!');
+// 	}
+// });
+
+var animalQuery = new Parse.Query(Animal);
+
+animalQuery.get('AagWh9YUMV', {
+	success: function(animal) {
+		console.log('animal is',animal);
+	},
+	error: function(object, error) {
+		console.log('OOPS!');
+	}
+});
+
+
+document.getElementById("uploadBtn").onchange = function () {
+    document.getElementById("uploadFile").value = this.value;
+};
